2054fdea83383ccf7fb1b1c393ce2978bc648365
Blasnjacobo <blasnjacobo05@gmail.com>
2023-11-11 11:57:22 -0700

    movies placed

--- src/components/Movies/Movie.js
+++ src/components/Movies/Movie.js
@@ -1,11 +1,26 @@
+/* eslint-disable no-lone-blocks */
 import React from 'react'
+import ItemPage from '../../pages/ItemPage'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 const Movie = (props) => {
   console.log('entering Movie')
   console.log(props.original_title)
+  const star = <FontAwesomeIcon icon={faStar} style={{ color: '#ffea00' }} />
+  const handleItem = () => {
+    { <ItemPage /> }
+  }
   return (
-    <div>
-      <div className='movieTitle'>{props.original_title}</div>
+    <div className='movie-section'>
+      <div>
+        <img className='movie-image' src={`https://image.tmdb.org/t/p/w500${props.poster_path}`} alt='movie poster generated' />
+      </div>
+      <div onClick={handleItem} className='movie-description'>
+        <span className='movie-star'>{star} {props.vote_average}</span>
+        <h4 className='movie-title'>{props.title}</h4>
+        <p className='movie-year'>Year: {props.release_date}</p>
+      </div>
     </div>
   )
 }
--- src/components/Movies/MovieList.js
+++ src/components/Movies/MovieList.js
@@ -6,10 +6,20 @@ import Movie from './Movie'
 const MovieList = ({ movieData }) => {
   // console.log('entering ListMovie')
   const pag1 = (movieData.pag1.results)
+  const pag2 = (movieData.pag2.results)
+  const pag3 = (movieData.pag3.results)
+  const pag4 = (movieData.pag4.results)
+  const pag5 = (movieData.pag5.results)
+  const pag6 = (movieData.pag6.results)
   console.log(pag1)
   return (
-    <div>
+    <div className='pag1-section'>
       {pag1.map((movie) => <Movie {...movie} />)}
+      {pag2.map((movie) => <Movie {...movie} />)}
+      {pag3.map((movie) => <Movie {...movie} />)}
+      {pag4.map((movie) => <Movie {...movie} />)}
+      {pag5.map((movie) => <Movie {...movie} />)}
+      {pag6.map((movie) => <Movie {...movie} />)}
     </div>
   )
 }
