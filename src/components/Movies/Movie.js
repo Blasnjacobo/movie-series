6cdd543be5d1d0d9f96d5894612008de611752d2
Blasnjacobo <blasnjacobo05@gmail.com>
2023-11-11 15:50:54 -0700

    movies placed with a big hole

--- src/components/Movies/Movie.js
+++ src/components/Movies/Movie.js
@@ -1,26 +1,31 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-lone-blocks */
 import React from 'react'
-import ItemPage from '../../pages/ItemPage'
+// import ItemPage from '../../pages/ItemPage'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 const Movie = (props) => {
   console.log('entering Movie')
-  console.log(props.original_title)
+  console.log(props)
+  const result = props.results
+  // console.log(props.original_title)
   const star = <FontAwesomeIcon icon={faStar} style={{ color: '#ffea00' }} />
-  const handleItem = () => {
-    { <ItemPage /> }
-  }
+  // const handleItem = () => {
+  //  { <ItemPage /> }
+  // }
   return (
-    <div className='movie-section'>
-      <div>
-        <img className='movie-image' src={`https://image.tmdb.org/t/p/w500${props.poster_path}`} alt='movie poster generated' />
-      </div>
-      <div onClick={handleItem} className='movie-description'>
-        <span className='movie-star'>{star} {props.vote_average}</span>
-        <h4 className='movie-title'>{props.title}</h4>
-        <p className='movie-year'>Year: {props.release_date}</p>
-      </div>
+    <div>
+      {result.map((movie) =>
+        <div className='movie-section'>
+          <img className='movie-image' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt='movie poster generated' />
+          <div className='movie-description'>
+            <span className='movie-star'>{star} {movie.vote_average}</span>
+            <h4 className='movie-title'>{movie.title}</h4>
+            <p className='movie-year'>Year: {movie.release_date}</p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/components/Movies/MoviesDisplay.js
+++ src/components/Movies/MoviesDisplay.js
@@ -4,11 +4,23 @@ import { useEffect, useState } from 'react'
 import MovieList from './MovieList'
 
 const MoviesDisplay = () => {
-  const [movieData, setMoviedata] = useState(window.localStorage.getItem('Data'))
+  const [movieData, setMoviedata] = useState([])
   // console.log('fase inicio')
 
   useEffect(() => {
-    // console.log('fase effect')
+    console.log('fase effect')
+    const arr = []
+    for (let i = 1; i <= 8; i++) {
+      arr.push(i)
+    }
+    const fetchData = async (index) => {
+      const initialData = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=${index}&sort_by=popularity.desc`, options)
+      const jsonResponse = await initialData.json()
+      setMoviedata(previousState => [...previousState, jsonResponse])
+      console.log(' fase inside fetch')
+    }
+
+    console.log('fase final effect')
     const options = {
       method: 'GET',
       headers: {
@@ -16,29 +28,9 @@ const MoviesDisplay = () => {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Nzc1MDYzZDI4Y2MyNmE2MGU3ZTQ3MzAxYzVhYTY3OSIsInN1YiI6IjY1NGJlMGIyNTMyYWNiNTMzOWZlNjIxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Axt69fPJjbo55Mx5ufNpWHxOkDQRWpeB_gRGKrBC_Xk'
       }
     }
-    Promise.all([
-      fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=${1}&sort_by=popularity.desc`, options),
-      fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=${2}&sort_by=popularity.desc`, options),
-      fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=${3}&sort_by=popularity.desc`, options),
-      fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=${4}&sort_by=popularity.desc`, options),
-      fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=${5}&sort_by=popularity.desc`, options),
-      fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=${6}&sort_by=popularity.desc`, options),
-      fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=${7}&sort_by=popularity.desc`, options),
-      fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=${8}&sort_by=popularity.desc`, options)
-    ])
-      .then(([r1, r2, r3, r4, r5, r6, r7, r8]) => Promise.all(([r1.json(), r2.json(), r3.json(), r4.json(), r5.json(), r6.json(), r7.json(), r8.json()])))
-      .then(([r1, r2, r3, r4, r5, r6, r7, r8]) => setMoviedata({
-        pag1: r1,
-        pag2: r2,
-        pag3: r3,
-        pag4: r4,
-        pag5: r5,
-        pag6: r6,
-        pag7: r7,
-        pag8: r8
-      }))
-    window.localStorage.setItem('Data', movieData)
+    arr.forEach(index => fetchData(index))
   }, [])
+  console.log(movieData)
   // console.log('fase antes de return')
   return (
     <div className='moviesDisplay-section'>
