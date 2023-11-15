7697de90c600daaf7a3950fc0661e4c8898e1c57
Blasnjacobo <blasnjacobo05@gmail.com>
2023-11-15 06:49:53 -0700

    trying

--- src/components/Upcoming/Homeupcoming.js
+++ src/components/Upcoming/Homeupcoming.js
@@ -4,14 +4,13 @@ import Upcoming from './Upcoming'
 // import useLocalStorage from './useLocalStorage'
 
 const Homeupcoming = () => {
-  const [Data, setData] = useState(window.localStorage.getItem('Data'))
+  const [Data, setData] = useState([])
   console.log('fase initial homecomming function')
 
   const getMovie = () => {
     fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=5775063d28cc26a60e7e47301c5aa679')
       .then(res => res.json())
       .then(json => setData(json.results))
-    window.localStorage.setItem('Data', Data)
   }
 
   useEffect(() => {
@@ -28,7 +27,7 @@ const Homeupcoming = () => {
       {console.log(Data)}
       {console.log(typeof Data)}
       {/* <Upcoming Data={Data} /> */}
-      {((typeof Data !== 'string') && Data.length > 0) && <Upcoming Data={Data} />}
+      {((typeof Data !== 'string') && (Data.length > 0) && (Data.length !== null)) && <Upcoming Data={Data} />}
     </div>
   )
 }
